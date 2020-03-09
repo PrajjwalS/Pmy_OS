87c3b2ebd8a9faefdf8de46ad2e98029a7c77564
PrajjwalS <prajjwals12th@gmail.com>
2020-03-09 23:47:45 +0530

    Changed boot sector to print string and loading address

--- bootsector/bs.asm
+++ bootsector/bs.asm
@@ -1,17 +1,19 @@
-;################### BOOT SECTOR #################
+
 	[org 0x7c00]			; specifies where bios will load the boot sector
 	
 	mov bx, HELLO_MSG		
 	call print_string		; calling the print utility
-
+	mov dx,0x7C00
+	call print_hex
 	jmp $				; infinite loop, jumping to current directory
 
-	%include "../asm_utilities/print_string_function.asm"		; including the print utility function file
+	%include "../asm_utilities/print.asm"		; including the print utility function file
 
 ;DATA
 HELLO_MSG:
-	db '###### Pmy OS ######',0 
+	db '###### Pmy_OS ######',10,13,'Boot Sector was loaded at:',0 
 	
 				
 	times 510-($-$$) db 0		; padding 0 till magic number
 	dw 0xaa55
+
