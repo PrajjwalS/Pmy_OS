a47c42e22305b89adf48a74cbceef42e6b6a8dbb
PrajjwalS <prajjwals12th@gmail.com>
2020-03-23 10:48:58 +0530

    new bootsector with load disk and checkup

--- bootsector/bs.asm
+++ bootsector/bs.asm
@@ -1,19 +1,48 @@
 
-	[org 0x7c00]			; specifies where bios will load the boot sector
+	[ORG 0x7c00]			; specifies where bios will load the boot sector
+
+	MOV	[BOOT_DRIVE],DL		; initally dl holdes the default readble disk
 	
-	mov bx, HELLO_MSG		
-	call print_string		; calling the print utility
-	mov dx,0x7C00
-	call print_hex
-	jmp $				; infinite loop, jumping to current directory
-
+	;moving stack to 0x8000
+	MOV 	BP,0x8000
+	MOV	SP,BP
+	
+	;printing HELLO_MSG and where the bootsector was loaded
+	MOV	BX, HELLO_MSG
+	CALL	print_string
+	MOV	DX,0x7C00
+	CALL	print_hex
+	
+	;loading the disk sectors at 0x9000 from ESreg (ES:BS i.e. ES+0x9000) 
+	MOV	BX,0x9000
+	MOV	DH,5		; read 5 sectors
+	MOV	DL,[BOOT_DRIVE]
+	CALL	load_disk
+	
+	;printing the loaded disk
+	mov	dx,[0x9000]
+	call	print_hex
+	mov	dx,[0x9000+512]
+	call	print_hex
+	mov	dx,[0x9000+1024]
+	call	print_hex
+	jmp 	$			; infinite loop, jumping to current directory
+	
+	
 	%include "../asm_utilities/print.asm"		; including the print utility function file
-
+	%include "../asm_utilities/load_disk.asm"
 ;DATA
 HELLO_MSG:
-	db '###### Pmy_OS ######',10,13,'Boot Sector was loaded at:',0 
-	
-				
-	times 510-($-$$) db 0		; padding 0 till magic number
-	dw 0xaa55
+	DB	'###### Pmy_OS ######',10,13,'Boot Sector was loaded at:',0 
+BOOT_DRIVE:
+	DB	0
 
+	TIMES	510-($-$$) DB 0		; padding 0 till magic number
+	DW	0xAA55
+
+	TIMES	256	DW	0xface
+	TIMES	256	DW	0xdada
+	TIMES	256	DW	0xabcd
+	TIMES	256	DW	0xdcba
+	TIMES	256	DW	0xacbd
+	
