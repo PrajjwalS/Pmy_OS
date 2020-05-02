5db37fd00b65e927c828f7fc91fb867f5a5a6841
PrajjwalS <prajjwals12th@gmail.com>
2020-05-02 21:32:05 +0530

    Added boot sector to that switches to 32bit mode and initializes GDT

--- bootsector/bs.asm
+++ bootsector/bs.asm
@@ -1,48 +1,31 @@
+; boot sector that enters 32 bit protected mode
+	[org 0x7c00]
 
-	[ORG 0x7c00]			; specifies where bios will load the boot sector
-
-	MOV	[BOOT_DRIVE],DL		; initally dl holdes the default readble disk
+	MOV	BP, 0x9000	; set stack
+	MOV	SP, BP
 	
-	;moving stack to 0x8000
-	MOV 	BP,0x8000
-	MOV	SP,BP
-	
-	;printing HELLO_MSG and where the bootsector was loaded
-	MOV	BX, HELLO_MSG
+	MOV	BX,	MSG_REAL_MODE
 	CALL	print_string
-	MOV	DX,0x7C00
-	CALL	print_hex
 	
-	;loading the disk sectors at 0x9000 from ESreg (ES:BS i.e. ES+0x9000) 
-	MOV	BX,0x9000
-	MOV	DH,5		; read 5 sectors
-	MOV	DL,[BOOT_DRIVE]
-	CALL	load_disk
-	
-	;printing the loaded disk
-	mov	dx,[0x9000]
-	call	print_hex
-	mov	dx,[0x9000+512]
-	call	print_hex
-	mov	dx,[0x9000+1024]
-	call	print_hex
-	jmp 	$			; infinite loop, jumping to current directory
-	
-	
-	%include "../asm_utilities/print.asm"		; including the print utility function file
-	%include "../asm_utilities/load_disk.asm"
-;DATA
-HELLO_MSG:
-	DB	'###### Pmy_OS ######',10,13,'Boot Sector was loaded at:',0 
-BOOT_DRIVE:
-	DB	0
+	CALL	SWITCH_TO_PM
+	JMP	$
 
-	TIMES	510-($-$$) DB 0		; padding 0 till magic number
-	DW	0xAA55
-
-	TIMES	256	DW	0xface
-	TIMES	256	DW	0xdada
-	TIMES	256	DW	0xabcd
-	TIMES	256	DW	0xdcba
-	TIMES	256	DW	0xacbd
+	%include "../asm_utilities_bios/print.asm"
+	%include "../gdt/gdt.asm"
+	%include "../asm_utilities_32bit/print.asm"
+	%include "../pm_switch/pm_switch.asm"
 	
+	[bits 32]
+	;; below is PM 
+BEGIN_PM:
+	MOV	EBX, MSG_PROT_MODE
+	CALL	print_string_pm;
+	JMP	$
+	
+; vars
+MSG_REAL_MODE	DB	"Started in 16-bit Real mode",0
+MSG_PROT_MODE	DB	"Successfully landed in 32-bit Protected Mode",0
+
+; padding
+	times 510-($-$$)	db 0
+	DW	0xaa55
